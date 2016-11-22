e3f8e2f8ed61653fb30da692d20d484932bbfc18
brenkr <robin.renker@iet-gibb.ch>
2016-11-22 20:30:52 +0100

    js replaced width css

--- stu/main.js
+++ stu/main.js
@@ -4,31 +4,15 @@ $(window).load(function () {
 $(window).resize(function () {
     main_resize();
 });
-$(window).resize(function () {
-    $('.center_vertical').each(function () {
-        var marg = ($(this).parent().height()-$(this).height())/2;
-        $(this).css({'margin-top':marg+"px"});
-    });
-    $('.center_horizontal').each(function () {
-        var marg = ($(this).parent().width()-$(this).width())/2;
-        $(this).css({'margin-left':marg+"px"});
-    })
-});
-
-/*##########################################*/
-var main_resize_dump = {
-    'x':0,
-    'y':0
-};
 /*##########################################*/
 function main_resize() {
     if($('#b').height() < $('#b').width()){
-        $('#b_cap').addClass("contvert");
-        $('#b_text').addClass("contvert");
+        $('#b_cap').addClass("contvert").removeClass("conthor1");
+        $('#b_text').addClass("contvert").removeClass("conthor2");
     }
     else{
-        $('#b_cap').addClass("conthor");
-        $('#b_text').addClass("conthor");
+        $('#b_cap').addClass("conthor1").removeClass("contvert");
+        $('#b_text').addClass("conthor2").removeClass("contvert");
     }
 }
 /*##########################################*/
