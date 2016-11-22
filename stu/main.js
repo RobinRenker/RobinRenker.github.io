1f42b89be89dcb28c69bbcf19e84785288b2eac0
RobinRenker <robin.renker@iet-gibb.ch>
2016-11-22 14:29:01 +0100

    CSS Update less js

--- stu/main.js
+++ stu/main.js
@@ -22,43 +22,14 @@ var main_resize_dump = {
 };
 /*##########################################*/
 function main_resize() {
-    if($('#b').height()/1000 < $('#b').width()/1200){ //700
-        $('#b_cap').height("100%");
-        $('#b_text').height("100%");
-        var percneeded = Math.floor($('#b_cap').height()*0.7/($('#b').width()/100));
-        $('#b_cap').width(percneeded+"%");
-        $('#b_text').width(100-percneeded+"%");
-        $('#b_cap_img')
-            .height('100%')
-            .width('auto');
+    if($('#b').height() < $('#b').width()){
+        $('#b_cap').addClass("contvert");
+        $('#b_text').addClass("contvert");
     }
     else{
-        $('#b_cap')
-            .width("100%")
-            .height("70%");
-        $('#b_text')
-            .width("100%")
-            .height("30%");
-        if($('#b_cap').width() > $('#b_cap').height()*0.7){
-            $('#b_cap_img')
-                .height('100%')
-                .width('auto');
-        }
-        else{
-            $('#b_cap_img')
-                .height('auto')
-                .width('100%');
-        }
+        $('#b_cap').addClass("conthor");
+        $('#b_text').addClass("conthor");
     }
 }
-/*##########################################*/
-var main_resize_watcher = setInterval(function () {
-    var cur_win_siz = base_window_size();
-    if(main_resize_dump['x'] != cur_win_siz[0] || main_resize_dump['y'] != cur_win_siz[1]){
-        main_resize_dump['x'] = cur_win_siz[0];
-        main_resize_dump['y'] = cur_win_siz[1];
-        $(window).trigger('resize');
-    }
-},200);
 /*##########################################*/
 
